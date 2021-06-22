69ef52523b3a9349957f2b222c0509d14d510833
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-22 16:39:32 +0900

    server config 획득 및 configMgr 코드 추가

--- ChatServer/Configs/ConfigMgr.cs
+++ ChatServer/Configs/ConfigMgr.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,22 @@ namespace ChatServer.Configs
 
         public static void Init()
         {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                var serverConfPath = appSettings.Get("ServerConfig");
+                var sconfStr = "";
+                using (var sr = new StreamReader(serverConfPath, Encoding.UTF8))
+                {
+                    sconfStr = sr.ReadToEnd();
+                }
+                JObject serverConfJson = JObject.Parse(sconfStr);
+                ServerConf = new ServerConf(serverConfJson);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
 
         }
     }
--- ChatServer/Configs/ServerConf.cs
+++ ChatServer/Configs/ServerConf.cs
@@ -2,9 +2,12 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ChatServer.Configs
 {
@@ -17,9 +20,53 @@ namespace ChatServer.Configs
         public string RSA_Public { get; set; }
         public string Dh_IV { get; set; }
 
-        protected ServerConf(JObject _jobj) : base(_jobj)
+        public ServerConf(JObject _jobj) : base(_jobj)
         {
-            GetJObjectFromProperties();
+            Init();
+        }
+
+        private void Init()
+        {
+            bool isNeedUpdate = false;
+            if (RSA_Private == "" || RSA_Public == "")
+            {
+                isNeedUpdate = true;
+
+                var csp = new RSACryptoServiceProvider();
+                var privateParam = csp.ExportParameters(true);
+                var publicParam = csp.ExportParameters(false);
+
+                using (var sw = new StringWriter())
+                {
+                    var xs = new XmlSerializer(typeof(RSAParameters));
+                    xs.Serialize(sw, publicParam);
+                    RSA_Public = sw.ToString();
+                }
+                using (var sw = new StringWriter())
+                {
+                    var xs = new XmlSerializer(typeof(RSAParameters));
+                    xs.Serialize(sw, privateParam);
+                    RSA_Private = sw.ToString();
+                }
+            }
+
+            if (Port == 0)
+            {
+                isNeedUpdate = true;
+                Port = 30000; // default port
+            }
+
+            if (Dh_IV == "")
+            {
+                isNeedUpdate = true;
+                //to do : Create new dh IV
+            }
+
+            if (isNeedUpdate)
+            {
+                var newServerConf = GetJObjectFromProperties();
+                //todo : WriteFile
+            }
         }
     }
 }
