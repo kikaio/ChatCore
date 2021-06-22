aaf7409dd5a34741f73b250ada569041b4bc4fb5
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-22 15:56:42 +0900

    Server Config 관련 json, class 추가

---
+++ ChatServer/Configs/ConfigMgr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Configs
+{
+    public static class ConfigMgr
+    {
+        public static ServerConf ServerConf { get; private set; }
+
+        public static void Init()
+        {
+
+        }
+    }
+}
---
+++ ChatServer/Configs/ServerConf.cs
@@ -0,0 +1,25 @@
+﻿using CoreNet.Utils.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Configs
+{
+    public class ServerConf : JsonicObj
+    {
+        public int Port { get; set; }
+        public int Max_Thread_Cnt { get; set; }
+        public string Category { get; set; }
+        public string RSA_Private { get; set; }
+        public string RSA_Public { get; set; }
+        public string Dh_IV { get; set; }
+
+        protected ServerConf(JObject _jobj) : base(_jobj)
+        {
+            GetJObjectFromProperties();
+        }
+    }
+}
