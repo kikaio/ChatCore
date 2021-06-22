f5a992fdce844055de0cf74ddb787c557ca9ec9e
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-22 22:53:47 +0900

    logger 및 state 변경 코드 삽입.

--- ChatClient/Sessions/UserSession.cs
+++ ChatClient/Sessions/UserSession.cs
@@ -1,6 +1,8 @@
 ﻿using ChatCore.Packets;
 using CoreNet.Networking;
 using CoreNet.Sockets;
+using CoreNet.Utils;
+using CoreNet.Utils.Loggers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,7 @@ namespace ChatClient.Sessions
     {
         private ESessionState curState;
         private Dictionary<ESessionState, SessionState> stateDict = new Dictionary<ESessionState, SessionState>();
-
+        private CoreLogger logger = new ConsoleLogger();
 
         public UserSession(long _sid, CoreSock _sock) : base(_sid, _sock)
         {
@@ -25,6 +27,14 @@ namespace ChatClient.Sessions
             stateDict[ESessionState.TRY_HELLO] = new State_Try_Hello(this);
             stateDict[ESessionState.TRY_GET_DH] = new State_Try_Get_dh(this);
             stateDict[ESessionState.CHATABLE] = new State_Chatable(this);
+        }
+
+        public void UpdateState(ESessionState _s)
+        {
+            if (curState == _s)
+                return;
+            logger.WriteDebug($"session state update from {curState.ToString()} to {_s.ToString()}");
+            curState = _s;
         }
 
         public void Dispatch_Ans(ChatPacket _cp)
