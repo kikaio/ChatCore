2cbb770b04e08fd728f8b9b5bbdd0cd073a8e429
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-24 17:07:30 +0900

    사용자 계정 관련 작업 중.

--- ChatServer/DataBases/Common/Account.cs
+++ ChatServer/DataBases/Common/Account.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Data.Entity;
+using MySql.Data.EntityFramework;
 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChatServer.DataBases.Common
 {
     [Serializable]
-    [Table(Account)]
+    [Table("Account")]
     public class Account
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
