37d5aa4b85eda16002c029b92395fe8744ffe7cd
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-24 23:28:46 +0900

    table create sql 추가

---
+++ ChatServer/DataBases/SQLs/CreateTableCommonDB.sql
@@ -0,0 +1,14 @@
+﻿
+USE `test_db`;
+
+CREATE TABLE `Account` (
+    `Id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `NickName` VARCHAR(50) NOT NULL UNIQUE, 
+    `Pw` varchar(100) NOT NULL,
+    `RegistDate` DateTime DEFAULT(UTC_TIMESTAMP()),
+    `UpdatedDate` DateTime DEFAULT(UTC_TIMESTAMP()),
+    `IsBlocked` BIT NOT NULL DEFAULT(0),
+    `IsSignOut` BIT NOT NULL DEFAULT(0)
+);
+
+
