10db52323e371260add62359dc7185c94b7f8828
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-24 21:26:27 +0900

    DBConf 정보 읽어오는 관련 코드 추가

--- ChatServer/Configs/ConfigMgr.cs
+++ ChatServer/Configs/ConfigMgr.cs
@@ -12,26 +12,57 @@ namespace ChatServer.Configs
     public static class ConfigMgr
     {
         public static ServerConf ServerConf { get; private set; }
-
+        public static Dictionary<string, DBConf> DbConfDict { get; private set; } = new Dictionary<string, DBConf>();
         public static void Init()
         {
             try
             {
                 var appSettings = ConfigurationManager.AppSettings;
                 var serverConfPath = appSettings.Get("ServerConfig");
-                var sconfStr = "";
-                using (var sr = new StreamReader(serverConfPath, Encoding.UTF8))
-                {
-                    sconfStr = sr.ReadToEnd();
-                }
-                JObject serverConfJson = JObject.Parse(sconfStr);
-                ServerConf = new ServerConf(serverConfJson);
+                var dbConfPath = appSettings.Get("DBConfig");
+
+                ReadyServerConfig(serverConfPath);
+                ReadyDbConfig(dbConfPath);
+
             }
             catch (Exception e)
             {
                 throw e;
             }
-
         }
+
+        private static void ReadyServerConfig(string _confPath)
+        {
+            var sconfStr = "";
+            using (var sr = new StreamReader(_confPath, Encoding.UTF8))
+            {
+                sconfStr = sr.ReadToEnd();
+            }
+            JObject serverConfJson = JObject.Parse(sconfStr);
+            ServerConf = new ServerConf(serverConfJson);
+        }
+
+        private static void ReadyDbConfig(string _confPath)
+        {
+            var confStr = "";
+            using (var sr = new StreamReader(_confPath, Encoding.UTF8))
+            {
+                confStr = sr.ReadToEnd();
+            }
+            JObject confObjDict = JObject.Parse(confStr);
+            foreach (var jc in confObjDict)
+            {
+                var confToken = jc.Value as JObject;
+                if (confToken == default(JObject))
+                {
+                    Console.WriteLine($"{jc.Key} is has not jobj");
+                    continue;
+                }
+                var conf = new DBConf(confToken);
+                DbConfDict[jc.Key] = conf;
+            }
+        }
+
+
     }
 }
---
+++ ChatServer/Configs/DBConf.cs
@@ -0,0 +1,47 @@
+﻿using CoreNet.Utils.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Configs
+{
+    public class DBConf : JsonicObj
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public string Db_Name { get; set; }
+
+        public bool IsNeedCreateFile = false;
+        public DBConf(JObject _jobj) : base(_jobj)
+        {
+            Init();
+        }
+        
+        private void Init()
+        {
+            bool IsNeedCreateFile = false;
+
+            if (Ip == "")
+            {
+                IsNeedCreateFile = true;
+                Ip = "127.0.0.1";
+            }
+
+            if (Port == 0)
+            {
+                IsNeedCreateFile = true;
+                Port = 3307;
+            }
+
+            if (Db_Name == "")
+            {
+                IsNeedCreateFile = true;
+                Db_Name = "TestDB";
+            }
+        }
+    }
+}
