a626174985ff91633d1742b7d3775f33d3a01d59
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-25 23:42:47 +0900

    Redis 연결 class 추가, Auth 관련 작업중.

---
+++ ChatServer/Redis/RedisAuth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Redis
+{
+    public class RedisAuth : RedisHelper
+    {
+        /* Auth 관련 정보들
+         * SessionToken
+         * 
+         */
+        public string SessionTokenKey { get; private set; } = "Auth:Token";
+
+        public RedisAuth(string _ip, int _port) 
+            : base("Auth", _ip, _port)
+        {
+        }
+
+        public void Init()
+        {
+            Connect();
+        }
+
+
+    }
+}
