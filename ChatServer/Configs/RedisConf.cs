16fd840b3e6e33f7705132f8e17df927b3664814
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-27 17:10:23 +0900

    Redis Config App setting, 관련 파일 추가.

---
+++ ChatServer/Configs/RedisConf.cs
@@ -0,0 +1,20 @@
+﻿using CoreNet.Utils.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer.Configs
+{
+    public class RedisConf : JsonicObj
+    {
+        public string Server { get; protected set; }
+        public int Port { get; protected set; }
+
+        public RedisConf(JObject _jobj) : base(_jobj)
+        {
+        }
+    }
+}
